0d4871ab24457c2f1b527db449712c3322f84849
root <root@ip-172-31-13-4.us-east-2.compute.internal>
2018-01-23 13:32:49 +0000

    class

--- app/Mail/EmailVerification.php
+++ app/Mail/EmailVerification.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\User;
 
-class EmailVerification.php extends Mailable
+class EmailVerification extends Mailable
 {
     use Queueable, SerializesModels;
 
